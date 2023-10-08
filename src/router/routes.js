0f4667ad0b2a7a7f936f4403edc9766e9f657ed3
bwilliamson55 <benjaminawilliamson@gmail.com>
2023-10-08 15:46:08 -0400

    Update router and routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,38 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      { path: "", component: () => import("pages/IndexPage.vue") },
+      {
+        path: "login",
+        name: "login",
+        component: () => import("pages/LoginPage.vue"),
+      },
+      {
+        path: "signup",
+        name: "signup",
+        component: () => import("pages/SignupPage.vue"),
+      },
+      {
+        path: "search",
+        component: () => import("pages/SearchPage.vue"),
+        meta: { requiresAuth: true },
+      },
+      {
+        path: "dashboard",
+        name: "dashboard",
+        component: () => import("pages/DashboardPage.vue"),
+        meta: { requiresAuth: true },
+      },
+    ],
   },
-
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
