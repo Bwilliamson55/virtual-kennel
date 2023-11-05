18e982791337ccaa08a0800c718ea9afe635685d
bwilliamson55 <benjaminawilliamson@gmail.com>
2023-11-05 17:49:01 -0500

    Update homepage for responsive image

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,8 +1,17 @@
 <template>
   <q-page class="flex flex-center q-pa-md column items-center justify-center">
-    <h1>Virtual Kennel</h1>
-    <p>This app helps manage your virtual kennel with ease.</p>
-    <img alt="Greyhounds racing" src="~assets/greyhounds-racing.png" />
+    <h1 class="text-center q-my-xs">Virtual Kennel</h1>
+    <p>
+      This is a proof of concept app to track your favorite greyhounds races and
+      results.
+    </p>
+    <q-img
+      class="flex flex-center"
+      alt="Greyhounds racing"
+      src="~assets/greyhounds-racing.png"
+      spinner-color="blue"
+      style="max-width: 450px"
+    />
     <div class="q-mt-md row justify-center">
       <q-btn rounded label="Login" class="q-mr-md" to="/login" />
       <q-btn rounded label="Sign Up" to="/signup" />
